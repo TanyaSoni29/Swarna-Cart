59fb51bcaa9613e7c7ca8caf609dddfae4f33d0f
TanyaSoni2912 <tanu139sona@gmail.com>
2025-04-14 14:51:37 +0530

    ads slice added and minor fix

--- src/core/redux/store.jsx
+++ src/core/redux/store.jsx
@@ -4,9 +4,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducer';
 import themeSettingSlice from './themeSettingSlice';
 import authReducer from './slices/authSlice';
+import adsReducer from './slices/adSlice';
+
 const store = configureStore({
 	reducer: {
 		auth: authReducer,
+		ads: adsReducer,
 		rootReducer: rootReducer,
 		themeSetting: themeSettingSlice,
 	},

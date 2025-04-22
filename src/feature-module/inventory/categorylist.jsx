4b42e7570b0d441feeae0e989a88bde76c925e0f
TanyaSoni2912 <tanu139sona@gmail.com>
2025-04-22 10:49:16 +0530

    Add Category Modal first one used

--- src/feature-module/inventory/categorylist.jsx
+++ src/feature-module/inventory/categorylist.jsx
@@ -12,6 +12,7 @@ import CommonFooter from '../../core/common/footer/commonFooter';
 import CommonDeleteModal from '../../core/common/modal/commonDeleteModal';
 import { useDispatch } from 'react-redux';
 import { refreshCategories } from '../../core/redux/slices/categoriesSlice';
+import AddCategoryList from '../../core/modals/inventory/addcategorylist';
 
 const CategoryList = () => {
 	const dispatch = useDispatch();
@@ -209,87 +210,7 @@ const CategoryList = () => {
 			</div>
 
 			{/* Add Category */}
-			<div
-				className='modal fade'
-				id='add-category'
-			>
-				<div className='modal-dialog modal-dialog-centered'>
-					<div className='modal-content'>
-						<div className='page-wrapper-new p-0'>
-							<div className='content'>
-								<div className='modal-header'>
-									<div className='page-title'>
-										<h4>Add Category</h4>
-									</div>
-									<button
-										type='button'
-										className='close bg-danger text-white fs-16'
-										data-bs-dismiss='modal'
-										aria-label='Close'
-									>
-										<span aria-hidden='true'>×</span>
-									</button>
-								</div>
-								<div className='modal-body'>
-									<form>
-										<div className='mb-3'>
-											<label className='form-label'>
-												Category<span className='text-danger ms-1'>*</span>
-											</label>
-											<input
-												type='text'
-												className='form-control'
-											/>
-										</div>
-										<div className='mb-3'>
-											<label className='form-label'>
-												Category Slug<span className='text-danger ms-1'>*</span>
-											</label>
-											<input
-												type='text'
-												className='form-control'
-											/>
-										</div>
-										<div className='mb-0'>
-											<div className='status-toggle modal-status d-flex justify-content-between align-items-center'>
-												<span className='status-label'>
-													Status<span className='text-danger ms-1'>*</span>
-												</span>
-												<input
-													type='checkbox'
-													id='user2'
-													className='check'
-													defaultChecked
-												/>
-												<label
-													htmlFor='user2'
-													className='checktoggle'
-												/>
-											</div>
-										</div>
-									</form>
-								</div>
-								<div className='modal-footer'>
-									<button
-										type='button'
-										className='btn me-2 btn-secondary fs-13 fw-medium p-2 px-3 shadow-none'
-										data-bs-dismiss='modal'
-									>
-										Cancel
-									</button>
-									<Link
-										to='#'
-										data-bs-dismiss='modal'
-										className='btn btn-primary fs-13 fw-medium p-2 px-3'
-									>
-										Add Category
-									</Link>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			<AddCategoryList />
 			{/* /Add Category */}
 
 			<EditCategoryList />
